26c6d436110be65ee767dd5c4b2a7144e736407f
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-01-06 10:08:52 +0530

    Added new rest routes

--- app/app.go
+++ app/app.go
@@ -10,8 +10,12 @@ import (
 // Start registers the handler functions and starts the server
 func Start() {
 	router := mux.NewRouter()
-	router.HandleFunc("/greet", greet)
-	router.HandleFunc("/customers", getAllCustomers)
+
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
 	err := http.ListenAndServe(":5000", router)
 	if err != nil {
 		log.Fatal(err)
